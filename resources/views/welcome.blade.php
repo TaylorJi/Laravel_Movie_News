5ecc0f1922427813d03665c2c626017260c4bb19
Siwoon Lim <59671717+SWL-01@users.noreply.github.com>
2023-04-06 00:27:12 -0700

    welcome page updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
     <title>Laravel</title>
 
     <style>
+        body {
+            font-family: 'figtree', sans-serif;
+        }
+
         .article-title {
             font-weight: bold;
             font-size: 22px;
@@ -30,7 +38,6 @@
             background-color: transparent;
             border-spacing: 0;
             margin-left: 20px;
-
         }
 
         .table td {
@@ -80,78 +87,95 @@
             width: 100px;
             height: 100px;
         }
+
+        .register-btn {
+            display: inline-block;
+            margin: 10px;
+            padding: 10px 20px;
+            font-size: 16px;
+            font-weight: bold;
+            text-align: center;
+            text-decoration: none;
+            background-color: green;
+            color: white;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        .login-btn {
+            display: inline-block;
+            margin: 10px;
+            padding: 10px 20px;
+            font-size: 16px;
+            font-weight: bold;
+            text-align: center;
+            text-decoration: none;
+            background-color: blue;
+            color: white;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
     </style>
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.4.3/css/bootstrap.min.css"
-        integrity="sha512-Zd9PhF5V7h4BnKQ7VklLox6Zn7P5Y5P5b7Vd9l9yf5VlvBNKjpuZ7aUoGzw3Zq3jKk6UcRH6dkR9nFQMr1T12Q=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- Scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.4.3/js/bootstrap.bundle.min.js"
-        integrity="sha512-i2I1DkXzjKto55TavTfrCPlTbZ46UbI1dSwW1YbGn0tdFCz9vE3f3WpZB0f/fJc1xGx8ZwWnMCr31rJtSE9IZQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
-<body class="antialiased bg-gray-100 dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-    <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center dark:bg-dots-lighter">
-        @if (Route::has('login'))
-            <div class="fixed top-0 right-0 sm:static sm:p-6 text-right">
-                @auth
-                    <a href="{{ url('/dashboard') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                @else
-                    <a href="{{ route('login') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
-                        in</a>
+<body>
+    <div style="margin: 80px; border: 3px solid #000000; padding: 20px; display: inline-block;">
+        <x-slot>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Articles') }}
+            </h2>
+        </x-slot>
+        <div
+            class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center dark:bg-dots-lighter">
+            @if (Route::has('login'))
+                <div class="fixed top-0 right-0 sm:static sm:p-6 text-right">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="btn btn-secondary">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="btn login-btn" style=".reg">Log in</a>
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-    </div>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="btn register-btn">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </div>
 
-    @if ($articles->isNotEmpty())
-        <img src="{{ $news[0]->logoUrl }}" class="article-logo" alt="{{ $articles[0]->title }}">
-        <h1 class="title">
-            {{ __('Newsletter #:number - :date', ['number' => $articles[0]->newsletter_id, 'date' => $articles[0]->updated_at->format('M d, Y')]) }}
-        </h1>
+        @if ($articles->isNotEmpty())
+            <img src="{{ $news[0]->logoUrl }}" class="article-logo" alt="{{ $articles[0]->title }}">
+            <h1 class="title">
+                {{ __('Newsletter #:number - :date', ['number' => $articles[0]->newsletter_id, 'date' => $articles[0]->updated_at->format('M d, Y')]) }}
+            </h1>
 
-        @foreach ($articles as $item)
+            @foreach ($articles as $item)
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="article-title">{{ $item->id }}. {{ $item->title }}</div>
+                                @if ($item->picUrl)
+                                    <img src="{{ $item->picUrl }}" alt="{{ $item->title }}" class="article-image">
+                                @endif
+                                <div class="article-content">
+                                    <div class="article-description">{!! $item->description !!}</div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            @endforeach
+        @else
             <table class="table">
                 <tbody>
                     <tr>
                         <td>
-                            <div class="article-title">{{ $item->id }}. {{ $item->title }}</div>
-                            @if ($item->picUrl)
-                                <img src="{{ $item->picUrl }}" alt="{{ $item->title }}" class="article-image">
-                            @endif
-                            <div class="article-content">
-                                <div class="article-description">{!! $item->description !!}</div>
-                            </div>
+                            No Articles, create one first
                         </td>
                     </tr>
                 </tbody>
             </table>
-        @endforeach
-    @else
-        <table class="table">
-            <tbody>
-                <tr>
-                    <td>
-                        No Articles, create one first
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    @endif
-</body>
+        @endif
+    </div>
 
 </html>
